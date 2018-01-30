c0aa0aeb1b91fa8fb7e8386d8b1966eadfac250e
sudakov <vsudakov@bk.ru>
2018-01-30 17:37:52 +0300

    add location

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'terminals.dart';
 import 'terminal.dart';
 import 'auth.dart';
 import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:location/location.dart';
 
 void main() => runApp(new MyApp());
 
@@ -60,10 +63,22 @@ bool sendingConnect = false;
 bool sendingPwd = false;
 bool loading = false;
 
+Map<String,double> _currentLocation;
+//StreamSubscription<Map<String,double>> _locationSubscription;
+Location _location = new Location();
 
 @override
 void initState() {
   super.initState();
+  initPlatformState();
+  //  _locationSubscription =
+        _location.onLocationChanged.listen((Map<String,double> result) {
+          setState(() {
+            _currentLocation = result;
+            //print ("_currentLocation = $_currentLocation");
+            //latitude: 37.34332805, longitude: -122.09343444, accuracy: 5.0, altitude: 0.0
+          });
+        });
   loading = true;
   cfg.initDB().then((Database db){
     print('Connected to db!');
@@ -81,7 +96,28 @@ void initState() {
 
 }
 
+// Platform messages are asynchronous, so we initialize in an async method.
+initPlatformState() async {
+  Map<String,double> location;
+  // Platform messages may fail, so we use a try/catch PlatformException.
 
+
+  try {
+    location = await _location.getLocation;
+  } on PlatformException {
+    location = null;
+  }
+
+  // If the widget was removed from the tree while the asynchronous platform
+  // message was in flight, we want to discard the reply rather than calling
+  // setState to update our non-existent appearance.
+  if (!mounted)
+    return;
+
+  setState(() {
+    _currentLocation = location;
+  });
+}
 
   @override
   Widget build(BuildContext context) {
@@ -111,6 +147,9 @@ void initState() {
   final Widget mainPage = new ListView(
   shrinkWrap: true,
   children: <Widget>[
+
+    _currentLocation == null ? new Text("") : new Text("Geo ${_currentLocation["latitude"]},${_currentLocation["longitude"]}"),
+
     new Container(
       height: 20.0,
       child: new Text("Задачи"),
