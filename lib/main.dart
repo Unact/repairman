6f00575a75d364d568e8b47703e21a1e3136b5ab
sudakov <vsudakov@bk.ru>
2018-02-01 14:06:53 +0300

    fix view

--- lib/main.dart
+++ lib/main.dart
@@ -51,36 +51,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-_MyHomePageState({this.cfg});
-int _currentIndex = 0;
-DbSynch cfg;
-bool sendingClose = false;
-bool sendingInit = false;
-bool sendingConnect = false;
-bool sendingPwd = false;
-bool loading = false;
+  _MyHomePageState({this.cfg});
+  int _currentIndex = 0;
+  DbSynch cfg;
+  bool sendingClose = false;
+  bool sendingInit = false;
+  bool sendingConnect = false;
+  bool sendingPwd = false;
+  bool loading = false;
+  double _distance = 0.0;
 
-@override
-void initState() {
-  super.initState();
+  @override
+  void initState() {
+    super.initState();
 
-  loading = true;
-  cfg.initDB().then((Database db){
-    print('Connected to db!');
-    cfg.getGeo();
-    cfg.saveGeo();
-//Это нужно, но не в таком виде
-/*
-    if (cfg.login == null || cfg.login == '' ||
-        cfg.password == null || cfg.password == '') {
-      _currentIndex = 1;
-    }
-*/
-  });
+    loading = true;
+    cfg.initDB().then((Database db){
+      print('Connected to db!');
+      cfg.getGeo();
+      cfg.saveGeo();
+  //Это нужно, но не в таком виде
+  /*
+      if (cfg.login == null || cfg.login == '' ||
+          cfg.password == null || cfg.password == '') {
+        _currentIndex = 1;
+      }
+  */
+    });
 
-}
-
-
+  }
   @override
   Widget build(BuildContext context) {
 
@@ -105,100 +104,91 @@ void initState() {
       },
     );
 
+    final Widget mainPage = new ListView(
+    shrinkWrap: true,
+    children: <Widget>[
+      new Container(
+        height: 20.0,
+        child: new Text("Задачи"),
+        color: Colors.grey.shade300,
+      ),
+      new GestureDetector(
+        onTap: () async
+        {
+          await Navigator.of(context).pushNamed(taskPageRoute);
+        },
+        child: new Container(
+          color: Colors.yellow,
+          child: new Text('Задачи'),
+          height: 40.0,
+        ),
+      ),
+      new Container(
+        height: 20.0,
+        child: new Text("Терминалы"),
+        color: Colors.grey.shade300,
+      ),
+      new GestureDetector(
+        onTap: () async
+        {
+          await Navigator.of(context).pushNamed(terminalsPageRoute);
+        },
+        child: new Container(
+          color: Colors.yellow,
+          child: new Text('Терминалы'),
+          height: 40.0,
 
-  final Widget mainPage = new ListView(
-  shrinkWrap: true,
-  children: <Widget>[
-    new Container(
-      height: 20.0,
-      child: new Text("Задачи"),
-      color: Colors.grey.shade300,
-    ),
-    new GestureDetector(
-     onTap: () async
-     {
-        await Navigator.of(context).pushNamed(taskPageRoute);
-     },
-     child: new Container(
-       color: Colors.yellow,
-       child: new Text('Задачи'),
-       height: 40.0,
-     ),
-   ),
-    new Container(
-      height: 20.0,
-      child: new Text("Терминалы"),
-      color: Colors.grey.shade300,
-    ),
-    new GestureDetector(
-     onTap: () async
-     {
-        await Navigator.of(context).pushNamed(terminalsPageRoute);
-     },
-     child: new Container(
-       color: Colors.yellow,
-       child: new Text('Терминалы'),
-       height: 40.0,
-
-     ),
-   ),
-    new Container(
-      height: 20.0,
-      child: new Text("Управление"),
-      color: Colors.grey.shade300,
-    ),
-  new GestureDetector(
-   onTap: () async
-   {
-      await Navigator.of(context).pushNamed(cgroupPageRoute);
-   },
-   child: new Container(
-     color: Colors.yellow,
-     child: new Text('ЗИПы'),
-     height: 40.0,
-
-   ),
- ),
- new Container(
-   height: 20.0,
-   child: new Text(""),
- ),
- new Container(
-   height: 20.0,
-   child: new Text(""),
- ),
-new RaisedButton(
-  color: Colors.blue,
-  onPressed: () async {
-
-    await cfg.fillDB();
-    print("completed...");
-
-  },
-  child: new Text('Обновить данные', style: new TextStyle(color: Colors.white)),
-
-
-),
-new RaisedButton(
-  color: Colors.red,
-  onPressed: () async {
-
-    await cfg.synchDB();
-    print("completed synchDB...");
-
-  },
-  child: new Text('Тест апдейт', style: new TextStyle(color: Colors.white)),
-
-)
-  ],
-);
-
+        ),
+      ),
+      new Container(
+        height: 20.0,
+        child: new Text("Управление"),
+        color: Colors.grey.shade300,
+      ),
+      new Row(
+        children: [
+          new Expanded(
+            flex: 5,
+            child: new Text("Геотрек:")
+          ),
+          new Expanded(
+            flex: 10,
+            child: new Text("$_distance", textAlign: TextAlign.end)
+          ),
+          new Expanded(
+            flex: 2,
+            child: new Text("км")
+          )
+        ]
+      ),
+      new Divider(),
+      new RaisedButton(
+        color: Colors.blue,
+        onPressed: () async {
+          await cfg.fillDB();
+          print("completed...");
+        },
+        child: new Text('Обновить данные', style: new TextStyle(color: Colors.white)),
+      ),
+      new Divider(),
+      new RaisedButton(
+        color: Colors.red,
+        onPressed: () async {
+          await cfg.synchDB();
+          print("completed synchDB...");
+        },
+        child: new Text('Тест апдейт', style: new TextStyle(color: Colors.white)),
+      )
+    ]);
 
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Техник")
       ),
-      body: _currentIndex==0?(mainPage):(new AuthPage(cfg: cfg)),
+      body: new Container(
+        padding: const EdgeInsets.all(8.0),
+        child: _currentIndex==0?(mainPage):(new AuthPage(cfg: cfg))
+      ),
       bottomNavigationBar: botNavBar,
     );
   }
