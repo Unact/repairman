fac233fe57b2a76254b74da20ada2902f879340c
sudakov <vsudakov@bk.ru>
2018-02-07 17:59:25 +0300

    add geo c

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,30 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import "NativeViewController.h"
+
+@interface AppDelegate()
+    @property (nonatomic, strong) NativeViewController *nativeViewController;
+@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
+  
+    NSLog(@"her 1");
+    
+    UIViewController *flutterViewController = [[FlutterViewController alloc] init];
+    self.nativeViewController = [[NativeViewController alloc] init]; //initWithRootViewController:flutterViewController];
+    //[self.nativeViewController setNavigationBarHidden:YES];
+    
+    //self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = flutterViewController;//self.nativeViewController;
+    //[self.window makeKeyAndVisible];
+    
   // Override point for customization after application launch.
+    [GeneratedPluginRegistrant registerWithRegistry:self];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
+
 
 @end
