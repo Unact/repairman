644e4825d27b90da89290feeb5f3dd110c591e14
sudakov <vsudakov@bk.ru>
2018-02-09 17:58:02 +0300

    release

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -9,51 +9,56 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
+
     self.locationManager = [[CLLocationManager alloc] init];
-    
+
     // Setup location tracker accuracy
     self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
-    
+
     // Distance filter
     self.locationManager.distanceFilter = 50.0;
-    
+
     // Assign location tracker delegate
     self.locationManager.delegate = self;
-    
-    
+
+
     [self.locationManager setPausesLocationUpdatesAutomatically:NO];
-    
     // For iOS9 we have to call this method if we want to receive location updates in background mode
     if([self.locationManager respondsToSelector:@selector(allowsBackgroundLocationUpdates)]){
         [self.locationManager setAllowsBackgroundLocationUpdates:YES];
     }
 
     [self.locationManager startMonitoringSignificantLocationChanges];
-    
+
     [GeneratedPluginRegistrant registerWithRegistry:self];
-    
+
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"Go to terminate");
+    [self.locationManager stopMonitoringSignificantLocationChanges];
+}
+
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
-    NSLog(@"Did update location");
-    
+    NSLog(@"Did update location!");
+
     // For real cases we should filter location array by accuracy
     // And check timestamp if you need real time tracking
     // By we don't do it here
     CLLocation *location = [locations lastObject];
     if (location == nil)
         return;
-    
+
     // Save location
     // [self addLocation:location];
-    
-    
+
+
 }
 
 - (void)addLocation:(nonnull CLLocation *)location {
-    
+
     // Use very simple way to store some simple data - saving to file
     // In real app if you want to save miltiple data - use databases.
     // We can't save location objects in file, so convert location into
