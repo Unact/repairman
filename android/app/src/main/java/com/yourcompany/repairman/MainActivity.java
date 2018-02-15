10c8262af3d00bf6d6397a87b3b9efabeeb1805e
sudakov <vsudakov@bk.ru>
2018-02-15 11:35:40 +0300

    ask permission

--- android/app/src/main/java/com/yourcompany/repairman/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/repairman/MainActivity.java
@@ -1,21 +1,41 @@
 package com.yourcompany.repairman;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.Manifest;
 
 public class MainActivity extends FlutterActivity{
   private LocationManager locationManager;
+
+  private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+  private boolean checkPermissions() {
+    int permissionState = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+    return permissionState == PackageManager.PERMISSION_GRANTED;
+  }
+
+  private void requestPermissions() {
+    ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+            REQUEST_PERMISSIONS_REQUEST_CODE);
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    if (!checkPermissions()) {
+      requestPermissions();
+    }
     locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
             1000 * 10, 10, locationListener);
     locationManager.requestLocationUpdates(
