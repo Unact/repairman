b218d6014d6c11671561c233069c8daedd443a7e
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2018-10-01 19:53:13 +0300

    DEV-37085

--- lib/app/pages/map_page.dart
+++ lib/app/pages/map_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math' show max, min;
 
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   final double _kPointBoundMove = 0.1;
-  final GlobalKey<YandexMapViewState> _mapKey = GlobalKey<YandexMapViewState>();
+  final GlobalKey<YandexMapState> _mapKey = GlobalKey<YandexMapState>();
 
   Widget _buildBody(BuildContext context) {
     List<Placemark> placemarks = widget.terminals.map((Terminal terminal) {
@@ -27,7 +28,7 @@ class _MapPageState extends State<MapPage> {
         iconName: 'lib/app/assets/images/placeicon.png',
         onTap: (lat, lon) async {
           await Navigator.push(context, MaterialPageRoute(builder: (context) => TerminalPage(terminal: terminal)));
-          await _mapKey.currentState?.refresh();
+          if (Theme.of(context).platform == TargetPlatform.iOS) await _mapKey.currentState?.refresh();
         }
       );
       return placemark;
@@ -39,15 +40,12 @@ class _MapPageState extends State<MapPage> {
     double maxLon = longitudes.reduce(max);
     double minLon = longitudes.reduce(min);
 
-    return YandexMapView(
+    return YandexMap(
       key: _mapKey,
-      afterMapRefresh: () async {
-        YandexMap yandexMap = _mapKey.currentState.yandexMap;
-
-        await yandexMap.showUserLayer(iconName: 'lib/app/assets/images/usericon.png');
-        await yandexMap.removePlacemarks();
-        await yandexMap.addPlacemarks(placemarks);
-        await yandexMap.setBounds(
+      onMapCreated: (YandexMapController controller) async {
+        await controller.showUserLayer(iconName: 'lib/app/assets/images/usericon.png');
+        await Future.wait(placemarks.map((Placemark placemark) => controller.addPlacemark(placemark)));
+        await controller.setBounds(
           northEastPoint: Point(latitude: maxLat + _kPointBoundMove, longitude: maxLon + _kPointBoundMove),
           southWestPoint: Point(latitude: minLat - _kPointBoundMove, longitude: minLon - _kPointBoundMove)
         );
