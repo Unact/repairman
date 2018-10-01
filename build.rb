76038f47d6aa1c0241e8f39640cdd3f9f1f9ce8c
sudakov <vsudakov@bk.ru>
2018-10-01 14:20:34 +0300

    pretty build

--- build.rb
+++ build.rb
@@ -1,3 +1,17 @@
+require 'yaml'
+require 'github_api'
+require 'dotenv/load'
+
+raise "version not set" if ARGV[0].nil? || ARGV[0] == ""
+
+pubspec = YAML.load_file('pubspec.yaml')
+
+old_version = pubspec["version"][0...pubspec["version"].index('+')]
+new_version = ARGV[0]
+
+text = File.read('pubspec.yaml')
+new_contents = text.gsub("version: #{old_version}", "version: #{new_version}")
+File.write('pubspec.yaml', new_contents)
 
 puts "------------------- start build --------------------------------"
 raise "build ios error" unless system("flutter build ios --release")
@@ -16,7 +30,24 @@ raise "create ipa error" unless system("xcodebuild -exportArchive -archivePath b
 
 puts "------------------ copy to unact.github.io ---------------------------"
 
-raise "shell error" unless system("cd ../unact.github.io/; \
-git reset --hard; git pull; \
-mv ../repairman/build/app/outputs/apk/release/app-release.apk . ; \
-mv ../repairman/build/ios/iphoneos/Runner.ipa/Runner.ipa .")
+text = File.read('../unact.github.io/manifest.plist')
+new_contents = text.gsub(old_version, new_version)
+File.write('../unact.github.io/manifest.plist', new_contents)
+
+text = File.read('../unact.github.io/index.html')
+new_contents = text.gsub(old_version, new_version)
+File.write('../unact.github.io/index.html', new_contents)
+
+github = Github.new basic_auth: ENV['TOKEN']
+
+release = github.repos.releases.create 'unact', 'repairman', tag_name: new_version
+
+github.repos.releases.assets.upload 'unact', 'repairman', release["id"],
+   '../repairman/build/ios/iphoneos/Runner.ipa/Runner.ipa',
+    name: "Runner.ipa",
+   content_type: "application/octet-stream"
+
+github.repos.releases.assets.upload 'unact', 'repairman', release["id"],
+      '../repairman/build/app/outputs/apk/release/app-release.apk',
+       name: "app-release.apk",
+      content_type: "application/octet-stream"
