efc5c2958d046d5e2be0e9260a0739fc040c98c4
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2020-09-18 11:20:10 +0300

    DEV-39762. Fix iOS build

--- ios/Runner/SwiftCompileFix.swift
+++
@@ -1,2 +0,0 @@
-// Fixes ld: library not found for -lswiftSwiftOnoneSupport for architecture x86_64
-// https://stackoverflow.com/questions/47157647/create-a-static-library-with-swift-and-xcode9-beta4
