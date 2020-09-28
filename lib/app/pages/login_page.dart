2e83ae0d9122a3de36f2a2808b7f57f00d06c6ed
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2020-09-28 13:41:01 +0300

    DEV-39781

--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -124,7 +124,7 @@ class _LoginPageState extends State<LoginPage> {
                       Padding(
                         padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
                         child: Container(
-                          width: 192,
+                          width: 160,
                           child: RaisedButton(
                             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
                             onPressed: _submit,
@@ -137,7 +137,7 @@ class _LoginPageState extends State<LoginPage> {
                       Padding(
                         padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
                         child: Container(
-                          width: 192,
+                          width: 160,
                           child: RaisedButton(
                             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
                             onPressed: _getNewPassword,
